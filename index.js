254fa0ed4e1b29cd1f4b6c47cc2c49553b547e49
Robin Millette <robin@millette.info>
2018-08-12 15:55:56 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// npm
+const { ret, Doit } = require('npm-git-links')
+
+console.log('RET:', ret)
+
+const x = new Doit(ret.repository, ret.headHash)
+console.log(x.browse)
+
+x.clearVersion()
+console.log(x.browse)
+
